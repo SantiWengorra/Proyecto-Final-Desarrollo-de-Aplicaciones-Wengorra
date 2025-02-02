efd32d62cd72959b97124345552dddf01d38c1ab
Santiago Wengorra <santi.wengorra@gmail.com>
2025-02-02 18:54:26 -0300

    Se eliminó código no utilizado

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import Navigator from './src/Navigation/navigator';
 import { Provider } from 'react-redux';
 import { store } from './src/store/index';
 import { init } from './src/config/dbSqlite';
-import { useEffect } from 'react';
 
 export default function App() {
 
